b7f87baa5d6ce1551db051d0d0dc6575f1abd9a2
kamaathedj <kamaudavikiruku@gmail.com>
2023-06-10 10:51:53 +0300

    Added play action to retrieve people first name

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable array-callback-return */
-import React from 'react';
-import { useStoreState } from 'easy-peasy';
+import { useStoreState, useStoreActions } from 'easy-peasy';
 
 function Product() {
   const people = useStoreState((state) => state.people);
+  const play = useStoreActions((actions) => actions.play);
   return (
     <div>
       <h1>product</h1>
       <h5>List of buyers</h5>
+      <button onClick={play} type="submit">play</button>
       <ul>
         {people.map((p) => (
           <div key={p.name}>
             <p>{p.name}</p>
             <p>{p.dob}</p>
+
           </div>
 
         ))}
